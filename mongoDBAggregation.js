fc7b3edd989a8fa73e3fe0b54decf7f98807b8e1
“ashuu7089” <pateriyaaashish255@gmail.com>
2024-10-01 12:12:30 +0530

    add lookup method

--- mongoDBAggregation.js
+++ mongoDBAggregation.js
@@ -147,3 +147,90 @@
         }
     }
 ]
+
+// 10 ) Who has registered the most recently ?
+[
+    {
+        $sort :{
+            registered: -1
+        }
+    },{
+        $limit : 4
+    },{
+        $project: {
+            name : 1,
+            registered: 1,
+            favoriteFruits: 1
+        }
+    }
+]
+
+// 11) categorized users by their favorite fruit ?
+
+[
+    {
+        $group : {
+          _id :"$favoriteFruits",
+          users: {
+            $push:  "$name"
+          }
+        }
+    }
+]
+
+// 12) How many users have 'ad' as the second tag in their list of tags?
+
+[
+    {
+        $match :{
+            "tags.1" : 'ad'
+        }
+    },{
+        $count : "tagSecondAd"
+    }
+]
+
+// 13) Find Users who have both enim and id as their tags.
+[
+    {
+        $match : {
+            tags :{
+                $all : ['enim', 'id']
+            }
+        }
+    }
+]
+
+// 14) List all companies located in the USA with their corresponding user count.
+
+[
+    {
+        $match : {
+            "company.location.country"  :"USA"
+        }
+    },{
+        $group : {
+            _id : "null", //company.title
+            userCount : {$sum: 1}
+        }
+    }
+]
+
+
+// 15) LOOKUP
+[
+    {
+        $lookup: {
+            from : "authors",
+            localField: "author_id",
+            foreignField: "_id",
+            as : 'author_details'
+        }
+    },{
+        $addFields:{
+            author_details : {
+                $first:"$author_details" // or $arrElemAt : ["author_details", 0]
+            }
+        }
+    }
+]
